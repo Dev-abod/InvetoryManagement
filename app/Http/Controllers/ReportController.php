14a170989ecbc06f90b749d0321fa5fe331610aa
AlaaAl-deen <alaaalselwi2020@gmail.com>
2025-12-23 11:18:12 +0300

    Reports: add method index in ReportConrtoller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -1,10 +1,29 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Item;
+use App\Models\Operation;
+use App\Models\Stock;
 
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
     //
+     /**
+     * Reports dashboard (cards page)
+     */
+    public function index()
+    {
+        return view('reports.index', [
+            // عدد المنتجات
+            'itemsCount' => Item::count(),
+
+            // عدد العمليات المخزنية
+            'operationsCount' => Operation::count(),
+
+            // إجمالي الأصناف الموجودة في المخزون (بدون حساب يدوي)
+            'stocksCount' => Stock::count(),
+        ]);
+    }
 }
